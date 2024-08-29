cbc7d2ff76313ef94177badca27cb73277d51695
chinnuanna123 <kurianchinnuanna@gmail.com>
2024-08-29 17:27:09 +0530

    modified

--- Calculator.java
+++ Calculator.java
@@ -3,12 +3,12 @@ public class Calculator {
     int x = 25;
     int y = 5;
     int sum = x + y;
-    System.out.println("The Sum is:"sum);
+    System.out.println("The Sum is:",sum);
     int sub = x - y;
-    System.out.println(sub);
+    System.out.println("The Substraction is:",sub);
     int div = x / y;
-    System.out.println(div);
+    System.out.println("The division is:",div);
     int multi = x * y;
-    System.out.println(multi);
+    System.out.println("The multiplication is:",multi);
   }
 }
