aa8e639ce61369b6f4addb07436f4b5f94976cb9
chinnuanna123 <kurianchinnuanna@gmail.com>
2024-08-29 16:39:32 +0530

    pushing the calculator java file

---
+++ Calculator.java
@@ -0,0 +1,14 @@
+public class Calculator {
+  public static void main(String[] args) {
+    int x = 25;
+    int y = 5;
+    int sum = x + y;
+    System.out.println(sum);
+    int sub = x - y;
+    System.out.println(sub);
+    int div = x / y;
+    System.out.println(div);
+    int multi = x * y;
+    System.out.println(multi);
+  }
+}
